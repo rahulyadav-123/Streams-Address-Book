b7acdfcd6f45ca0ba0ff6f32f1b53568aa4b34f9
rahul yadav <ry219613@gmail.com>
2021-11-11 17:10:15 +0530

    Added method to edit the exsiting contact

--- com/bridgelabz/addressbook/AddressBook.java
+++ com/bridgelabz/addressbook/AddressBook.java
@@ -1,25 +1,74 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook {
+	
+	List<ContactPerson> list = new ArrayList<ContactPerson>();
+	ContactPerson person = new ContactPerson();
+	Scanner sc = new Scanner(System.in);
+	
+	public void AddContact() {
+		System.out.println("Enter number of contacts you need to create :");
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			createContact();
+		}
+	}
+	
+	public void createContact() {
+		System.out.println("Enter First Name : ");
+		String firstName = sc.next();
+		System.out.println("Enter Last Name : ");
+		String lastName = sc.next();
+		System.out.println("Enter Address : ");
+		String address = sc.next();
+		System.out.println("Enter City : ");
+		String city = sc.next();
+		System.out.println("Enter State : ");
+		String state = sc.next();
+		System.out.println("Enter Zip : ");
+		int zip = sc.nextInt();
+		System.out.println("Enter Mobile Number : ");
+		long phoneNumber = sc.nextLong();
+		System.out.println("Enter Email : ");
+		String email = sc.next();
+		person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		list.add(person);
+		printContact();		
+	}
+	
+	public void printContact() {
+		System.out.println("Contact Details Are: ");
+		System.out.println("Name 	     : "  + person.getFirstName() + " " + person.getLastName() + "\n"
+                + "Address      : "  + person.getAddress() + "\n"
+                + "City         : "  + person.getCity() + "\n"
+                + "State        : "  + person.getState() + "\n"
+                + "ZipCode      : "  + person.getZip() + "\n"
+                + "MobileNumber : "  + person.getPhonenumber() + "\n"
+                + "EmailId      : "  + person.getEmail() + "\n" );
+		
+	}
+	
+	public void editContact() {
+		System.out.println("Enter the name to edit the contact: ");
+		String editName = sc.next();
+		for(int i = 0; i < list.size(); i++) {
+			String name = list.get(i).getFirstName();
+			if(name.equalsIgnoreCase(editName)) {
+				createContact();			
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book");
-		String firstName = "Rahul";
-		String lastName = "Yadav";
-		String address = "Near hanuman temple";
-		String city = "pune";
-		String state = "Maharashtra";
-		int pinCode = 400001;
-		long mobileNumber = 8899221100l;
-		String emailId = "ry219613@gmail.com";
-		System.out.println("Name 	     : "  + firstName + " " + lastName + "\n"
-                + "Address      : "  + address + "\n"
-                + "City         : "  + city + "\n"
-                + "State        : "  + state + "\n"
-                + "ZipCode      : "  + pinCode + "\n"
-                + "MobileNumber : "  + mobileNumber + "\n"
-                + "EmailId      : "  + emailId + "\n" );
+		AddressBook addressBook = new AddressBook();
+		addressBook.AddContact();
+		addressBook.editContact();
 	}
-	}
+}
 
 	
 		
