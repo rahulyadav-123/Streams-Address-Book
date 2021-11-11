ae87bed5f9edc57cfb071776fedbf66e9809bc4e
rahul yadav <ry219613@gmail.com>
2021-11-11 17:30:24 +0530

    Finding the duplicate entry while adding the person name

--- com/bridgelabz/addressbook/AddressBook.java
+++ com/bridgelabz/addressbook/AddressBook.java
@@ -14,7 +14,15 @@ public class AddressBook {
 		System.out.println("Enter number of contacts you need to create :");
 		int n = sc.nextInt();
 		for(int i = 0; i < n; i++) {
-			createContact();
+			System.out.println("Enter the first name of person");
+            String fName = sc.next();
+            if (fName.equalsIgnoreCase(person.getFirstName())) {
+                System.out.println("The entered person is already exist.");
+            } else {
+                System.out.println("Enter the contact details of person ");
+                createContact();
+                System.out.println("contact added Successfully");
+            }
 		}
 	}
 	
@@ -87,8 +95,31 @@ public class AddressBook {
 	            System.out.println("Name does not exit");
 		 }
 	}
+	
+	public void choice() {	
+		while (true) {
+			System.out.println("Enter \n 1 To add The contact \n 2 To edit the contact \n 3 To delete the contact \n 4 to exit");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    addContact();
+                    break;
+                case 2:
+                    editContact();
+                    break;
+                case 3:
+                    deleteContact();
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    continue;
+            }
+		}
+	}
 }
-
 	
 		
 	
--- com/bridgelabz/addressbook/AddressBookMain.java
+++ com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,6 +1,28 @@
 package com.bridgelabz.addressbook;
 
-public class AddressBookMain {
-	
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class AddressBookMain {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to AddressBook Mangament System");
+		Map<String, AddressBook> map = new HashMap<>();
+        System.out.println("Enter Number of address book you want to add");
+        int count = scan.nextInt();
+        int i = 0;
+        while (i < count) {
+            System.out.println("Enter book name");
+            String bookName = scan.next();
+            AddressBook addressBook = new AddressBook();
+            addressBook.choice();
+			map.put(bookName, addressBook);
+            i++;
+        }
+        System.out.println(map);
+    }
+   
+	
 }
+
